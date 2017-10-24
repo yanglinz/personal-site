4a01af3ade6270683ab056a7db19057392081a9c
Yanglin Zhao <yz1876@gmail.com>
2017-10-23 22:47:57 -0400

    Remove bio component and css utils

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import Link from "gatsby-link";
 import { Container } from "react-responsive-grid";
 
-import { rhythm, scale } from "../utils/typography";
-
 class Template extends React.Component {
   render() {
     const { location, children } = this.props;
@@ -16,13 +14,7 @@ class Template extends React.Component {
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0
-          }}
-        >
+        <h1>
           <Link
             style={{
               boxShadow: "none",
@@ -37,33 +29,13 @@ class Template extends React.Component {
       );
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: "Montserrat, sans-serif",
-            marginTop: 0,
-            marginBottom: rhythm(-1)
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: "none",
-              textDecoration: "none",
-              color: "inherit"
-            }}
-            to={"/"}
-          >
-            Gatsby Starter Blog
-          </Link>
+        <h3>
+          <Link to={"/"}>Gatsby Starter Blog</Link>
         </h3>
       );
     }
     return (
-      <Container
-        style={{
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-        }}
-      >
+      <Container>
         {header}
         {children()}
       </Container>
