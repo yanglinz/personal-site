afb1ef8612a3e46c120c966dde8ed8f44529a972
Yanglin Zhao <yz1876@gmail.com>
2017-10-29 21:37:21 -0400

    Style intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,16 +3,19 @@ import React from "react";
 class Intro extends React.Component {
   render() {
     return (
-      <div>
-        <h1 css={{ fontSize: "100px", margin: 0 }}>Hi.</h1>
-        <p css={{ fontSize: "99px", fontWeight: "bold", margin: 0 }}>
-          I'm Yanglin.
-        </p>
-        <p css={{ fontSize: "60px", fontWeight: "bold", margin: 0 }}>
-          I currently work at PBS as a frontend engineer, and I’m passionate
-          about building interactive applications.
-        </p>
-      </div>
+      <section className="intro">
+        <div className="l-wrapper">
+          <div className="l-inner-narrow l-spacing">
+            <p className="f1" css={{ margin: "0.5em 0" }}>
+              Hi. I'm Yanglin.
+            </p>
+            <p className="f2" css={{ margin: "0.5em 0" }}>
+              I currently work at PBS as a frontend engineer, and I’m passionate
+              about building interactive applications.
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
