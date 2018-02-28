f78eeabe9f6c58f00fd2a776b02b4795c07421ae
Yanglin Zhao <yz1876@gmail.com>
2018-02-28 17:40:25 -0500

    Create basic crypto component

---
+++ packages/experiment-activity-pulse/src/crypto-info.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class CryptoInfo extends Component {
+  render() {
+    const title = "Bitcoin";
+    const price = 2204;
+    const changeAbs = -233;
+    const changePercent = "-1.2%";
+    const time = "Jul 16";
+
+    return (
+      <div className="CryptoInfo">
+        <div className="CryptoInfo-title">{title}</div>
+        <div className="CryptoInfo-price-change">{changeAbs}</div>
+        <div className="CryptoInfo-price">{price}</div>
+        <div className="CryptoInfo-price">{time}</div>
+
+        <div className="CryptoInfo-plot">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default CryptoInfo;
