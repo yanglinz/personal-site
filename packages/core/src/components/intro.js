c9bbb1d11b270fdfb76c98b18fef0257ceff6df0
Yanglin Zhao <yz1876@gmail.com>
2018-05-02 17:00:47 -0400

    Add software to intro

--- packages/core/src/components/intro.js
+++ packages/core/src/components/intro.js
@@ -10,7 +10,7 @@ class Intro extends React.Component {
           <div className="l-inner-narrow">
             <h2 className="Intro-primary">Hi. My name is Yanglin.</h2>
             <h3 className="Intro-secondary">
-              I'm a fullstack dev and I build interactive applications.
+              I'm a fullstack software dev and I build interactive applications.
             </h3>
           </div>
         </div>
