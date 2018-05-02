2b44254c8eb080f7bd30439c0e6d9e1872486e47
Yanglin Zhao <yz1876@gmail.com>
2018-05-02 16:13:04 -0400

    Move header into layout

--- packages/core/src/pages/blog.js
+++ packages/core/src/pages/blog.js
@@ -3,17 +3,10 @@ import Link from "gatsby-link";
 import get from "lodash/get";
 import Helmet from "react-helmet";
 
-import Header from "../components/header";
-
 import "../styles/main.scss";
 
 export const pageQuery = graphql`
   query BlogQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(
       filter: { fileAbsolutePath: { glob: "**/src/posts/**" } }
       sort: { fields: [frontmatter___date], order: DESC }
@@ -32,14 +25,12 @@ export const pageQuery = graphql`
 
 class Blog extends React.Component {
   render() {
-    const { data, location } = this.props;
-    const siteTitle = get(data, "site.siteMetadata.title");
+    const { data } = this.props;
     const posts = get(data, "allMarkdownRemark.edges");
 
     return (
       <div>
         <Helmet title={get(data, "site.siteMetadata.title")} />
-        <Header siteTitle={siteTitle} location={location} />
         {posts.map(post => {
           if (post.node.path !== "/404/") {
             const title = get(post, "node.frontmatter.title") || post.node.path;
