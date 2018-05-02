f416778051e52f0a6b100d5e2e75e7398740495a
Yanglin Zhao <yz1876@gmail.com>
2018-05-02 15:49:04 -0400

    Simplify header

--- packages/core/src/components/header.js
+++ packages/core/src/components/header.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
 
-import SearchBox from "./search-box";
-
-import mainLogo from "./header-logo.svg";
-import githubLogo from "./header-github-logo.svg";
-import linkedinLogo from "./header-linkedin-logo.svg";
-
 import "./header.scss";
 
 function Brand(props) {
@@ -16,14 +10,6 @@ function Brand(props) {
       <h1>
         <Link to="/">
           <div className="Brand-inner">
-            <div className="Brand-logo">
-              <img
-                className="Brand-logo-img"
-                src={mainLogo}
-                alt="Yanglin Zhao"
-              />
-            </div>
-
             <div className="Brand-tag-line f-serif">
               <span className="Brand-tag-top">Personal Site</span>
               <span className="Brand-tag-mid"> of </span>
@@ -32,24 +18,6 @@ function Brand(props) {
           </div>
         </Link>
       </h1>
-    </div>
-  );
-}
-
-function Links(props) {
-  return (
-    <div className="Links">
-      <div className="Links-link Links-link-github">
-        <a href="#">
-          <img className="Links-img" src={githubLogo} alt="Github" />
-        </a>
-      </div>
-
-      <div className="Links-link Links-link-linkedin">
-        <a href="#">
-          <img className="Links-img" src={linkedinLogo} alt="Linkedin" />
-        </a>
-      </div>
     </div>
   );
 }
@@ -85,14 +53,6 @@ class Header extends React.Component {
             <div className="Header-l-wrapper">
               <div className="Header-l-brand">
                 <Brand siteTitle={siteTitle} />
-              </div>
-
-              <div className="Header-l-search">
-                <SearchBox />
-              </div>
-
-              <div className="Header-l-links">
-                <Links />
               </div>
             </div>
           </div>
