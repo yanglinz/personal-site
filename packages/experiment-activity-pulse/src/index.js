c7940bfcebe85b937036362b24a3180ceaf07681
Yanglin Zhao <yz1876@gmail.com>
2018-05-02 15:36:52 -0400

    Add prettier to root scripts

--- packages/experiment-activity-pulse/src/index.js
+++ packages/experiment-activity-pulse/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './app';
+import App from "./app";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
