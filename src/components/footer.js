ac8333e0903fbfeb558fbed2a883765521a9bd2e
Yanglin Zhao <yz1876@gmail.com>
2018-05-27 11:08:44 -0400

    Add footer content

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <div className="l-wrapper">
-        {/* <p>Yanglin Zhao &copy; 2018</p> */}
+        <p>Yanglin Zhao &copy; 2018</p>
       </div>
     </footer>
   );
