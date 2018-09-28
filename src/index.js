89111fb068b22affdf13c7dfaf60f000c167fc85
Yanglin Zhao <yz1876@gmail.com>
2018-09-27 20:28:28 -0400

    Add background image

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ import "normalize.css/normalize.css";
 import "./global.css";
 import "../layouts/partials/header.css";
 import "../layouts/partials/home.css";
+
+import { initBackgroundImage } from "../layouts/partials/home";
+
+function main() {
+  initBackgroundImage();
+}
+
+function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === "complete"
+      : document.readyState !== "loading"
+  ) {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+ready(main);
