9111312be3e110cadf81e8002b999d7c0334f5e2
Yanglin Zhao <yz1876@gmail.com>
2018-11-26 21:14:59 -0500

    Add example markdown post

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,30 +3,38 @@ module.exports = {
     title: "Gatsby Default Starter"
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    // Sources
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`
       }
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+        name: "pages",
+        path: `${__dirname}/src/pages`
       }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`
+      }
+    },
+
+    // Plugins
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-remark",
+
+    // Transformers
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark"
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ]
 };
