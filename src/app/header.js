24b175a27e3d0803637061c3e993eef2315602da
Yanglin Zhao <yz1876@gmail.com>
2018-11-26 23:05:03 -0500

    Convert header

--- src/app/header.js
+++ src/app/header.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import "./header.scss";
+
 function Header(props) {
   const { siteTitle } = props;
   return (
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+    <div className="Header">
+      <div className="l-wide">
+        <div className="Header-brand">
+          <h1 className="Header-title">
+            <Link className="Header-title-link" to="/">
+              {siteTitle}
+            </Link>
+          </h1>
+          <span className="Header-subtitle">hello (at) yanglinzhao.com</span>
+        </div>
+      </div>
     </div>
   );
 }
