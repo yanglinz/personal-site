e2a3fdd4518fd061516cb3debba55df704114895
Yanglin Zhao <yz1876@gmail.com>
2018-11-27 21:25:23 -0500

    Migrate home intro

---
+++ src/app/intro.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./intro.scss";
+
+function Intro() {
+  return (
+    <div class="Intro">
+      <div class="l-wide">
+        <div class="Intro-border-top" />
+        <h1 class="Intro-text">Software Tinkerer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
