118f159e612d7d1a997f27480255fe81c362b6e5
Yanglin Zhao <yz1876@gmail.com>
2018-12-10 20:23:41 -0600

    Convert article to gatsby

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+
+import Layout from "../app/layout";
+
+import "./blog-post.scss";
 
 export const BLOG_POST_QUERY = graphql`
   query BlogPostTemplate($slug: String!) {
@@ -7,6 +11,9 @@ export const BLOG_POST_QUERY = graphql`
       id
       excerpt
       html
+      fields {
+        slug
+      }
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
@@ -17,7 +24,30 @@ export const BLOG_POST_QUERY = graphql`
 
 function BlogPostTemplate(props) {
   const post = props.data.markdownRemark;
-  return <h1>{post.frontmatter.title}</h1>;
+  return (
+    <Layout>
+      <main>
+        <article className="BlogPost">
+          <div className="l-wide">
+            <div className="BlogPost-metadata">
+              <h1 className="BlogPost-title">
+                <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+              </h1>
+
+              <time className="BlogPost-date">{post.frontmatter.date}</time>
+            </div>
+          </div>
+
+          <div className="l-wide">
+            <div
+              className="BlogPost-content"
+              dangerouslySetInnerHTML={{ __html: post.html }}
+            />
+          </div>
+        </article>
+      </main>
+    </Layout>
+  );
 }
 
 export default BlogPostTemplate;
