f9d1bf4ecef765941176f965ff8a21de4162a332
Yanglin Zhao <yz1876@gmail.com>
2019-07-20 11:41:28 -0400

    Grid provider should contain the layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,14 @@
-import React, { Suspense } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
 import Header from "../components/header";
 import Footer from "../components/footer";
-import * as env from "../app/env";
+import GridProvider from "../styles/grid-provider";
 
 import "normalize.css/normalize.css";
 import "../styles/global.scss";
 import "./layout.scss";
-
-const GridOverlay = React.lazy(() => import("../styles/grid-overlay"));
 
 const QUERY = graphql`
   query SiteTitleQuery {
@@ -27,32 +25,29 @@ const QUERY = graphql`
 function Layout(props) {
   const { children } = props;
   return (
-    <StaticQuery
-      query={QUERY}
-      render={data => (
-        <React.Fragment>
-          <Helmet>
-            <html lang="en" />
-            <title>{data.site.siteMetadata.title}</title>
-            <meta
-              name="description"
-              content={data.site.siteMetadata.description}
-            />
-            <meta name="author" content={data.site.siteMetadata.author} />
-          </Helmet>
-          <div className="Layout">
-            <Header siteTitle={data.site.siteMetadata.title} />
-            <div className="Layout-content">{children}</div>
-            <Footer />
-          </div>
-          {env.DEV ? (
-            <Suspense fallback={null}>
-              <GridOverlay />
-            </Suspense>
-          ) : null}
-        </React.Fragment>
-      )}
-    />
+    <GridProvider>
+      <StaticQuery
+        query={QUERY}
+        render={data => (
+          <React.Fragment>
+            <Helmet>
+              <html lang="en" />
+              <title>{data.site.siteMetadata.title}</title>
+              <meta
+                name="description"
+                content={data.site.siteMetadata.description}
+              />
+              <meta name="author" content={data.site.siteMetadata.author} />
+            </Helmet>
+            <div className="Layout">
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <div className="Layout-content">{children}</div>
+              <Footer />
+            </div>
+          </React.Fragment>
+        )}
+      />
+    </GridProvider>
   );
 }
 
