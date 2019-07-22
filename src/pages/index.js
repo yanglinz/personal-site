656c990d28b88316f0dae9021bedb8c706f42a10
Yanglin Zhao <yanglinz@users.noreply.github.com>
2019-07-21 20:28:10 -0400

    Add intro section (#19)

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,9 +58,23 @@ function parsePosts(data) {
 }
 
 function IndexPage(props) {
+  const wave = (
+    <span role="img" aria-label="Waving hand">
+      &#x1F44B;
+    </span>
+  );
+
   return (
     <Layout>
       <Intro />
+      <Section title="About">
+        <p className="About-text">
+          Hi {wave}. I'm a software engineer based in Washington DC. I mainly
+          make things that run in browsers. I currently work at{" "}
+          <a href="https://www.pbs.org/">PBS</a> as a Principal Engineer to help
+          make high quality media accessible.
+        </p>
+      </Section>
       <Section title="Blog Posts">
         <PostList posts={parsePosts(props.data)} />
       </Section>
