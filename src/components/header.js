daafa860f6cc8d64bcaaf627d6ac2a7304627bc6
Yanglin Zhao <yz1876@gmail.com>
2020-02-05 21:41:01 -0500

    Add icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import GithubSVG from "./icons/github";
+
 import "./header.scss";
 
 function Header(props) {
@@ -8,13 +10,21 @@ function Header(props) {
   return (
     <div className="Header">
       <div className="l-wide">
-        <div className="Header-brand">
-          <h1 className="Header-title">
-            <Link className="Header-title-link" to="/">
-              {siteTitle}
-            </Link>
-          </h1>
-          <p className="Header-subtitle">hi (at) yanglinzhao.com</p>
+        <div className="Header-container">
+          <div className="Header-brand">
+            <h1 className="Header-title">
+              <Link className="Header-title-link" to="/">
+                {siteTitle}
+              </Link>
+            </h1>
+            <p className="Header-subtitle">hi (at) yanglinzhao.com</p>
+          </div>
+
+          <div className="Header-links">
+            <a href="https://github.com/yanglinz">
+              <GithubSVG />
+            </a>
+          </div>
         </div>
       </div>
     </div>
