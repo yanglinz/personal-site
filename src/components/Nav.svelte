57ff82eda2219b1403417c15574da768cf833bc6
Yanglin Zhao <yz1876@gmail.com>
2020-06-13 15:57:34 -0400

    Remove about route

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -56,14 +56,6 @@
       </a>
     </li>
     <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        about
-      </a>
-    </li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li>
       <a
         rel="prefetch"
         aria-current={segment === 'blog' ? 'page' : undefined}
