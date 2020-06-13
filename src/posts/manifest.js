eb44dec4ed9b589f3c01ffde9823d74f8cc4a30c
Yanglin Zhao <yz1876@gmail.com>
2020-06-13 15:56:23 -0400

    Remove a couple of fixture posts

--- src/posts/manifest.js
+++ src/posts/manifest.js
@@ -4,28 +4,14 @@ const posts = [
     slug: "what-is-sapper",
     html: `Hello world!`
   },
-
   {
     title: "How to use Sapper",
     slug: "how-to-use-sapper",
     html: `Hello world!`
   },
-
   {
     title: "Why the name?",
     slug: "why-the-name",
-    html: `Hello world!`
-  },
-
-  {
-    title: "How is Sapper different from Next.js?",
-    slug: "how-is-sapper-different-from-next",
-    html: `Hello world!`
-  },
-
-  {
-    title: "How can I get involved?",
-    slug: "how-can-i-get-involved",
     html: `Hello world!`
   }
 ];
@@ -34,11 +20,6 @@ const postsBySlug = {};
 posts.forEach(p => {
   postsBySlug[p.slug] = p;
 });
-
-// const lookup = new Map();
-// posts.forEach((post) => {
-//   lookup.set(post.slug, JSON.stringify(post));
-// });
 
 export function getManifest() {
   return posts;
