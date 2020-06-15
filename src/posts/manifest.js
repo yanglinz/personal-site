2f62ac030d5ac79690be46b9c095df448f0e29cb
Yanglin Zhao <yz1876@gmail.com>
2020-06-14 22:58:50 -0400

    Use gray-matter to parse frontmatter in posts

--- src/posts/manifest.js
+++ src/posts/manifest.js
@@ -1,49 +1,21 @@
 const path = require("path");
 const fs = require("fs");
 
+const matter = require("gray-matter");
+
 const rootDir = path.join(__dirname, "../../..");
 const postsDir = path.join(rootDir, "src/posts");
 
-function parseMarkdown(slug) {
-  const postPath = path.join(postsDir, slug, "index.md");
+function parseFrontmatter(slug) {
   return new Promise((resolve, reject) => {
-    unified()
-      .use(remarkParse)
-      .use(remarkHtml)
-      .use(remarkHighlight)
-      .use(remarkFrontmatter)
-      .use(remarkFrontmatterExtract, { yaml })
-      .process(vfile.readSync(postPath), (err, file) => {
-        if (err) {
-          reject(err);
-        } else {
-          const postData = {
-            ...file.data,
-            slug,
-            html: file.contents
-          };
-          resolve(postData);
-        }
-      });
-  });
-}
-
-function parseMarkdownFrontmatter(slug) {
-  const postPath = path.join(postsDir, slug, "index.md");
-  return new Promise((resolve, reject) => {
-    unified()
-      .use(remarkParse)
-      .use(remarkHtml)
-      .use(remarkFrontmatter)
-      .use(remarkFrontmatterExtract, { yaml })
-      .process(vfile.readSync(postPath), (err, file) => {
-        if (err) {
-          reject(err);
-        } else {
-          const postData = { ...file.data, slug };
-          resolve(postData);
-        }
-      });
+    const postPath = path.join(postsDir, slug, "index.md");
+    const postContent = fs.readFileSync(postPath, "utf8");
+    const metadata = matter(postContent);
+    if (metadata.data) {
+      resolve({ ...metadata.data, slug });
+    } else {
+      reject();
+    }
   });
 }
 
@@ -51,13 +23,9 @@ export async function getManifest() {
   let posts = await fs.promises.readdir(postsDir, { withFileTypes: true });
   posts = posts.filter(d => d.isDirectory());
   posts = posts.map(p => p.name);
-  return await Promise.all(posts.map(parseMarkdownFrontmatter));
+  return await Promise.all(posts.map(parseFrontmatter));
 }
 
 export async function hasPost(slug) {
   return fs.existsSync(path.join(postsDir, slug));
-}
-
-export async function getPost(slug) {
-  return await parseMarkdown(slug);
 }
