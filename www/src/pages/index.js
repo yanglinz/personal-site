9ce2088cf2c9bf616e7391d1f2fbb88ca9557d9e
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-06-19 00:30:45 -0400

    Move existing code in a www workspace (#38)
    * Remove netlify.toml
    
    * Move existing package into www
    
    * Add volta
    
    * Add yarn.lock
    
    * Add a start script
    
    * Deploy from www
    
    * www
    
    * Change hi -> hello

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -69,7 +69,7 @@ function IndexPage(props) {
       <Intro />
       <Section title="About">
         <p className="About-text">
-          Hi {wave}. I'm a software engineer based in Washington DC. I mainly
+          Hello {wave}. I'm a software engineer based in Washington DC. I mainly
           make things that run in browsers. I currently work at{" "}
           <a href="https://www.pbs.org/">PBS</a> as a Principal Engineer to help
           make high quality media accessible.
