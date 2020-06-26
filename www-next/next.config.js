d04a61c903b1ea3c933be6de774cd4f6625b0f4d
Yanglin Zhao <yz1876@gmail.com>
2020-06-25 22:10:05 -0400

    Add export command

--- www-next/next.config.js
+++ www-next/next.config.js
@@ -31,5 +31,15 @@ module.exports = withPrefresh({
     }
 
     return config;
+  },
+
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" }
+    };
   }
 });
