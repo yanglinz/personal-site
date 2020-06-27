a09fb93ac8a29b27934c26c592070fab329a029d
Yanglin Zhao <yz1876@gmail.com>
2020-06-27 17:35:45 -0400

    Add package.json dev script

--- www-sapper/src/components/Content/index.svelte
+++ www-sapper/src/components/Content/index.svelte
@@ -3,6 +3,11 @@
   import { parsePortableText } from "./parser.js";
 
   export let portableText;
+
+  const debugBefore = portableText;
+  const debugAfter = parsePortableText(portableText);
+  console.log(debugBefore);
+  console.log(debugAfter);
 </script>
 
 <ContentNodes nodes={parsePortableText(portableText)} />
