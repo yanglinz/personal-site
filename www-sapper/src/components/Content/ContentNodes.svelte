3601b75c36247ffb9aa1afcd6a5f0441ddf2d11e
Yanglin Zhao <yz1876@gmail.com>
2020-06-27 17:24:04 -0400

    Add whitespace comment

--- www-sapper/src/components/Content/ContentNodes.svelte
+++ www-sapper/src/components/Content/ContentNodes.svelte
@@ -16,6 +16,14 @@
   }
 </script>
 
+<!--
+  Note that svelte has some quirks when it comes to outputting
+  whitespaces that matters when we're dealing with links and periods.
+  This really ugly hack/workaround with comments makes sure that we're
+  not outputting markup that affects rendering in the markup.
+  See https://github.com/sveltejs/svelte/issues/3080.
+-->
+
 {#each nodes as node}<!--
 --><svelte:component this={getParentComponent(node.type)}><!--
 -->{#if node.children}<!--
