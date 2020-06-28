e997ea748cd2ed400946769c402760ca9af47b03
Yanglin Zhao <yz1876@gmail.com>
2020-06-28 14:17:59 -0400

    Add image metadata to portable text

--- www-sapper/src/routes/posts/[slug].json.js
+++ www-sapper/src/routes/posts/[slug].json.js
@@ -1,5 +1,4 @@
 import { getPost } from "../../posts/manifest.js";
-import { getHighlightMarkup } from "../../posts//highlight.js";
 
 export async function get(req, res, next) {
   const { slug } = req.params;
@@ -13,18 +12,6 @@ export async function get(req, res, next) {
   } else {
     statusCode = 200;
     data = { message: "Not found" };
-  }
-
-  // If the post data has raw code snippets,
-  // augment it with  syntax highlighting markup
-  if (data.bodyRaw) {
-    data.bodyRaw = data.bodyRaw.map(d => {
-      if (d._type === "code") {
-        d.markup = getHighlightMarkup(d.code, d.language);
-      }
-
-      return d;
-    });
   }
 
   res.writeHead(statusCode, { "Content-Type": "application/json" });
