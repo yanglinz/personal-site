bca85f1f102e154cee667323a8200818c4eba431
Yanglin Zhao <yz1876@gmail.com>
2020-07-01 22:52:21 -0400

    Add about

--- www-sapper/src/routes/index.svelte
+++ www-sapper/src/routes/index.svelte
@@ -1,5 +1,19 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch("posts.json")
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
 <script>
   import Intro from "../components/Intro.svelte";
+  import IndexSection from "../components/IndexSection.svelte";
+
+  export let posts;
+  console.log(posts);
 </script>
 
 <svelte:head>
@@ -7,3 +21,14 @@
 </svelte:head>
 
 <Intro />
+
+<IndexSection title="About">
+  <p class="About-text">
+    Hello
+    <span role="img" aria-label="Waving hand">&#x1F44B;.</span>
+    I'm a software engineer based in Washington DC. I mainly make things that
+    run in browsers. I currently work at
+    <a href="https://www.pbs.org/">PBS</a>
+    as a Principal Engineer to help make high quality media accessible.
+  </p>
+</IndexSection>
