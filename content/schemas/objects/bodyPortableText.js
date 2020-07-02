df65951c590785cfc056bafa8dc2fd0fb6be8d93
Yanglin Zhao <yz1876@gmail.com>
2020-07-02 09:54:23 -0400

    Remove some comments

--- content/schemas/objects/bodyPortableText.js
+++ content/schemas/objects/bodyPortableText.js
@@ -6,10 +6,6 @@ export default {
     {
       type: "block",
       title: "Block",
-      // Styles let you set what your user can mark up blocks with. These
-      // corrensponds with HTML tags, but you can set any title or value
-      // you want and decide how you want to deal with it where you want to
-      // use your content.
       styles: [
         { title: "Normal", value: "normal" },
         { title: "H1", value: "h1" },
@@ -22,15 +18,11 @@ export default {
         { title: "Bullet", value: "bullet" },
         { title: "Numbered", value: "number" }
       ],
-      // Marks let you mark up inline text in the block editor.
       marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting by editors.
         decorators: [
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" }
         ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
             name: "link",
@@ -48,9 +40,6 @@ export default {
       },
       of: [{ type: "authorReference" }]
     },
-    // You can add additional types here. Note that you can't use
-    // primitive types such as 'string' and 'number' in the same array
-    // as a block type.
     {
       type: "mainImage",
       options: { hotspot: true }
