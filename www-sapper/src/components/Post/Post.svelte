813b77ed936b0161a3b054032841268278092e7d
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-07-02 14:46:34 -0400

    Style posts (#56)

---
+++ www-sapper/src/components/Post/Post.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let post;
+</script>
+
+<main>
+  <article class="BlogPost">
+    <div class="l-wide">
+      <div class="BlogPost-metadata">
+        <h1 class="BlogPost-title">
+          <a rel="prefetch" href="posts/{post.slug}">{post.title}</a>
+        </h1>
+
+        <time class="BlogPost-date">{post.date || '1.1.11'}</time>
+      </div>
+    </div>
+
+    <div class="l-narrow">
+      <div class="BlogPost-content">
+        <slot />
+      </div>
+    </div>
+  </article>
+</main>
