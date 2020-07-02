33dcf56a4eb8eb04addfa216eba04fc6310b56a5
Yanglin Zhao <yz1876@gmail.com>
2020-07-02 10:29:13 -0400

    Remove old post list

--- www-sapper/src/routes/posts/index.json.js
+++
@@ -1,7 +0,0 @@
-import { getPostsList } from "../../content/manifest.js";
-
-export async function get(req, res) {
-  const data = await getPostsList();
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
-}
