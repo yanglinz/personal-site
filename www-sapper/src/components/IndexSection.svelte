70a9482c2d59be2d5b3c663084a42a7a577bc226
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-07-02 07:13:10 -0400

    Implement home about section and post list section (#53)
    * Add about
    
    * Add post list

---
+++ www-sapper/src/components/IndexSection.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let title;
+</script>
+
+<div class="Section">
+  <div class="l-wide">
+    <h2 class="Section-title">{title}</h2>
+    <slot />
+  </div>
+</div>
--- www-sapper/src/routes/index.svelte
+++ www-sapper/src/routes/index.svelte
@@ -1,5 +1,19 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch("posts.json")
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
 <script>
   import Intro from "../components/Intro.svelte";
+  import IndexSection from "../components/IndexSection.svelte";
+  import PostList from "../components/PostList.svelte";
+
+  export let posts;
 </script>
 
 <svelte:head>
@@ -7,3 +21,18 @@
 </svelte:head>
 
 <Intro />
+
+<IndexSection title="About">
+  <p class="About-text">
+    Hello
+    <span role="img" aria-label="Waving hand">&#x1F44B;.</span>
+    I'm a software engineer based in Washington DC. I mainly make things that
+    run in browsers. I currently work at
+    <a href="https://www.pbs.org/">PBS</a>
+    as a Principal Engineer to help make high quality media accessible.
+  </p>
+</IndexSection>
+
+<IndexSection title="Blog Posts">
+  <PostList {posts} />
+</IndexSection>
