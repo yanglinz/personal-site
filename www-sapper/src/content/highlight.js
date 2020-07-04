ce35d638ab3a18ccbe2b7ac5c27d2d2e41779b93
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-07-04 11:11:37 -0400

    Syntax highlighting for python (#61)

--- www-sapper/src/content/highlight.js
+++ www-sapper/src/content/highlight.js
@@ -1,6 +1,15 @@
 const Prism = require("prismjs");
+const loadLanguages = require("prismjs/components/");
+
+loadLanguages(["python"]);
 
 export function getHighlightMarkup(source, lang) {
-  const grammar = Prism.languages[lang];
-  return Prism.highlight(source, grammar, lang);
+  let markup;
+  try {
+    const grammar = Prism.languages[lang];
+    markup = Prism.highlight(source, grammar, lang);
+  } catch (e) {
+    // Do nothing
+  }
+  return markup;
 }
