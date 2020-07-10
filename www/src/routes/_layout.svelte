4de26d612cf5346e15618b7ce6f607b3e3ff1381
Yanglin Zhao <yz1876@gmail.com>
2020-07-10 19:50:58 -0400

    Implement analytics tracking with fathom

--- www/src/routes/_layout.svelte
+++ www/src/routes/_layout.svelte
@@ -1,8 +1,11 @@
 <script>
+  import Analytics from "../components/Analytics.svelte";
   import Layout from "../components/Layout.svelte";
 
   export let segment;
 </script>
+
+<Analytics />
 
 <Layout {segment}>
   <slot />
