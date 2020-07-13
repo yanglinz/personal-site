a02d3ead22383899c40ca73dcc049a134323c9d9
Yanglin Zhao <yz1876@gmail.com>
2020-07-12 22:25:07 -0400

    Fix footer link mobile tap spacing

--- www/src/components/Footer.svelte
+++ www/src/components/Footer.svelte
@@ -3,17 +3,16 @@
 </script>
 
 <div class="Footer">
-  <div class="Footer-links">
-    <a href="https://github.com/yanglinz">
-      <GithubIcon fill="#486581" />
-    </a>
-  </div>
-  <div class="Footer-links">
-    <a href="sitemap.xml">Sitemap</a>
-  </div>
   <div class="l-wide">
+    <div class="Footer-links">
+      <a href="https://github.com/yanglinz">
+        <GithubIcon fill="#486581" />
+      </a>
+    </div>
     <div class="Footer-copyright">
-      Yanglin Zhao &copy; {new Date().getFullYear()}
+      <span>Yanglin Zhao &copy; {new Date().getFullYear()}</span>
+      <span>|</span>
+      <a href="sitemap.xml">Sitemap</a>
     </div>
   </div>
 </div>
