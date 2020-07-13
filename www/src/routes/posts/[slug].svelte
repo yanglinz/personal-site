b3cbde5e8ab28ad5d08bbc9ef407d99b5c92f298
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-07-12 21:46:45 -0400

    Add a meta description tag for each post (#69)

--- www/src/routes/posts/[slug].svelte
+++ www/src/routes/posts/[slug].svelte
@@ -16,10 +16,23 @@
   import Content from "../../components/Content/index.svelte";
 
   export let post;
+
+  let excerpt;
+  try {
+    if (post.excerptRaw) {
+      excerpt = post.excerptRaw[0].children[0].text;
+    }
+  } catch (e) {
+    // Do nothing
+  }
 </script>
 
 <svelte:head>
   <title>{post.title}</title>
+
+  {#if excerpt}
+    <meta name="description" content={excerpt} />
+  {/if}
 </svelte:head>
 
 <Post {post}>
