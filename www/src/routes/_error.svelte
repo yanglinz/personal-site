e2249fb0524d864421d6d62cd8644cfbd64f705d
Yanglin Zhao <yz1876@gmail.com>
2020-07-12 22:10:20 -0400

    Add a better 404 screen

--- www/src/routes/_error.svelte
+++ www/src/routes/_error.svelte
@@ -1,18 +1,12 @@
 <script>
+  import Error from "../components/Error/Error.svelte";
+
   export let status;
   export let error;
-
-  const dev = process.env.NODE_ENV === "development";
 </script>
 
 <svelte:head>
-  <title>{status}</title>
+  <title>{status} | Yanglin Zhao</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-  <pre>{error.stack}</pre>
-{/if}
+<Error {status} {error} />
