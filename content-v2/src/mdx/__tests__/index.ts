8f066d0f0b5b856fd04de214b9ebbf1825d5e41b
Yanglin Zhao <yz1876@gmail.com>
2020-08-16 22:11:19 -0400

    Add ability to parse paragraph

--- content-v2/src/mdx/__tests__/index.ts
+++ content-v2/src/mdx/__tests__/index.ts
@@ -1,14 +1,27 @@
 import * as mdx from "../index";
 
 describe("mdx parser", () => {
-  it("should parse simple markdown", async () => {
-    const mdxString = "# Hello world!";
+  it("should parse headings", async () => {
+    const mdxString = "# Some heading";
     const ast = await mdx.getSvelteAST(mdxString);
 
     expect(ast).toEqual({
       type: "root",
       children: [
-        { type: "h1", children: [{ type: "text", value: "Hello world!" }] }
+        { type: "h1", children: [{ type: "text", value: "Some heading" }] }
+      ]
+    });
+  });
+
+  it("should parse headings + text", async () => {
+    const mdxString = "# Some heading\nSome text.";
+    const ast = await mdx.getSvelteAST(mdxString);
+
+    expect(ast).toEqual({
+      type: "root",
+      children: [
+        { type: "h1", children: [{ type: "text", value: "Some heading" }] },
+        { type: "paragraph", children: [{ type: "text", value: "Some text." }] }
       ]
     });
   });
