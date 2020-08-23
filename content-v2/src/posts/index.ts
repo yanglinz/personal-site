17cb3c9feeaaa8360c79e0cd5b3fbf68fe3bc216
Yanglin Zhao <yz1876@gmail.com>
2020-08-22 22:55:31 -0400

    Add build script

--- content-v2/src/posts/index.ts
+++ content-v2/src/posts/index.ts
@@ -1,7 +1,16 @@
+import fs from "fs";
+import path from "path";
+
 import { SvelteAST } from "../mdx";
+import { getSvelteAST } from "../mdx";
 
 interface Post {
   id: string;
+  title: string;
+  date: string;
+}
+
+interface PostMetadata {
   title: string;
   date: string;
 }
@@ -11,4 +20,49 @@ interface PostDetail {
   title: string;
   date: string;
   body: SvelteAST;
+}
+
+function getFileContent(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(String(data));
+      }
+    });
+  });
+}
+
+export async function getPostList(): Promise<Post[]> {
+  const files = fs.readdirSync(__dirname, { withFileTypes: true });
+  const postDirectories = files
+    .filter(f => f.isDirectory())
+    .map(async f => {
+      const postId = f.name;
+      const metadata = await getPostMetadata(postId);
+      return { id: postId, title: metadata.title, date: metadata.date };
+    });
+  return await Promise.all(postDirectories);
+}
+
+async function getPostMetadata(postId: string): Promise<PostMetadata> {
+  const metadata = await getFileContent(
+    path.resolve(__dirname, postId, "metadata.json")
+  );
+  return JSON.parse(metadata);
+}
+
+export async function getPostDetail(postId: string): Promise<PostDetail> {
+  const postMdx = await getFileContent(
+    path.resolve(__dirname, postId, "index.md")
+  );
+  const postAst = await getSvelteAST(postMdx);
+  const metadata = await getPostMetadata(postId);
+  return {
+    id: postId,
+    title: metadata.title,
+    date: metadata.date,
+    body: postAst
+  };
 }
