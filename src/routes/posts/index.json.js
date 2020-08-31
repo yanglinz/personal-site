5666ea586c7c581b1acb0baef6ed78fa22b786b9
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-08-30 23:04:26 -0400

    Return post list from markdown posts (#89)
    * Get list of posts from built manifest of markdown posts
    
    * Properly return error
    
    * Use the date variable
    
    * Skip tests for now

--- src/routes/posts/index.json.js
+++ src/routes/posts/index.json.js
@@ -1,7 +1,18 @@
-import { getPostsList } from "../../content/manifest.js";
+import fs from "fs";
+import path from "path";
 
 export async function get(req, res) {
-  const data = await getPostsList();
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
+  const manifestPath = path.resolve(
+    __dirname,
+    "../../../content/build/_manifest.json"
+  );
+  fs.readFile(manifestPath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(err));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    }
+  });
 }
