cff71c8ee414bf69b87120b688ad9f823442d10e
Yanglin Zhao <yz1876@gmail.com>
2020-08-30 22:53:44 -0400

    Get list of posts from built manifest of markdown posts

--- src/routes/posts/index.json.js
+++ src/routes/posts/index.json.js
@@ -1,7 +1,17 @@
-import { getPostsList } from "../../content/manifest.js";
+import fs from "fs";
+import path from "path";
 
 export async function get(req, res) {
-  const data = await getPostsList();
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
+  const manifestPath = path.resolve(
+    __dirname,
+    "../../../content/build/_manifest.json"
+  );
+  fs.readFile(manifestPath, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    }
+  });
 }
