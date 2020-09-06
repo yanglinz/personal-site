6cf474e50370f1c64434df07dab46a2fb0eb19ea
Yanglin Zhao <yz1876@gmail.com>
2020-09-06 12:23:37 -0400

    Inline rest of content to src

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -1,8 +1,8 @@
 import fs from "fs";
 import path from "path";
 
-import { getPostList, getPostDetail } from "../content/posts/index";
-import { getImageComponent } from "../content/images/index";
+import { getPostList, getPostDetail } from "../posts/index";
+import { getImageComponent } from "../images/index";
 
 async function writeFile(filePath: string, content: string) {
   return new Promise((resolve, reject) => {
@@ -41,7 +41,7 @@ async function writeFile(filePath: string, content: string) {
   const imageComponent = await getImageComponent();
   const imageComponentPath = path.resolve(
     __dirname,
-    "../content/images/ImageManifest.svelte"
+    "../images/ImageManifest.svelte"
   );
   await writeFile(imageComponentPath, imageComponent);
 })();
