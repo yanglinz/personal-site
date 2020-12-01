0c0af76769ac3171600d4b827cebe7dd3d8704dd
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-12-01 00:19:59 -0500

    Serve dynamic routes in vercel (#109)
    * Omit builds
    
    * Use vercel next
    
    * Try omitting vercel.json
    
    * Use next config js
    
    * Next.js config
    
    * Choose ssr
    
    * Try using trailing slash

---
+++ next.config.js
@@ -0,0 +1,46 @@
+const withPrefresh = require("@prefresh/next");
+const preact = require("preact");
+const withPreact = require("next-plugin-preact");
+
+const preactConfig = withPreact({
+  experimental: {
+    modern: true,
+  },
+});
+
+module.exports = {
+  // Use preact instead of React
+  ...preactConfig,
+
+  // Some routing configs
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/using-honeycomb/",
+        destination: "/posts/using-honeycomb/",
+        permanent: true,
+      },
+      {
+        source: "/cloudflare-workers/",
+        destination: "/posts/cloudflare-workers/",
+        permanent: true,
+      },
+      {
+        source: "/service-boundaries/",
+        destination: "/posts/service-boundaries/",
+        permanent: true,
+      },
+      {
+        source: "/component-local-reducer/",
+        destination: "/posts/component-local-reducer/",
+        permanent: true,
+      },
+      {
+        source: "/hello-blog/",
+        destination: "/posts/hello-blog/",
+        permanent: true,
+      },
+    ];
+  },
+};
