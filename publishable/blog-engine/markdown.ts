32ef8faf0176f92e344c2fea472ac28bb46e42c0
Yanglin Zhao <yz1876@gmail.com>
2020-12-04 00:13:52 -0500

    Update snapshots

--- publishable/blog-engine/markdown.ts
+++ publishable/blog-engine/markdown.ts
@@ -2,6 +2,8 @@ import unified from "unified";
 import remarkParse from "remark-parse";
 import mdastToHast from "mdast-util-to-hast";
 
+// TODO: Filter AST?
+
 export async function getHast(markdown: string): Promise<any> {
   const processor = unified().use(remarkParse);
   const mdast = processor.parse(markdown);
