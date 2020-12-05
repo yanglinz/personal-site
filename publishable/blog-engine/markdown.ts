f976215948112d0372a0b4f75ec69e2cf05ed068
Yanglin Zhao <yz1876@gmail.com>
2020-12-04 23:36:34 -0500

    Remove unused comment

--- publishable/blog-engine/markdown.ts
+++ publishable/blog-engine/markdown.ts
@@ -2,8 +2,6 @@ import unified from "unified";
 import remarkParse from "remark-parse";
 import mdastToHast from "mdast-util-to-hast";
 
-// TODO: Filter AST?
-
 export async function getHast(markdown: string): Promise<any> {
   const processor = unified().use(remarkParse);
   const mdast = processor.parse(markdown);
