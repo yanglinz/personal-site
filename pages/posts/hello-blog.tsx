c4aa773c89c16da498ae4b07d56b2b2c319a1955
Yanglin Zhao <yz1876@gmail.com>
2020-12-04 22:49:34 -0500

    More structure

--- pages/posts/hello-blog.tsx
+++ pages/posts/hello-blog.tsx
@@ -2,13 +2,22 @@ import React from "react";
 
 import {
   ContentScope,
-  MarkdownHTML,
+  ContentHTML,
 } from "../../publishable/blog-engine/components/post";
 
-export default function Page() {
+const postId = "hello-blog";
+
+export async function getStaticProps() {
+  const manifest = await import("../../publishable/blog-engine/manifest");
+  const content = await manifest.getPostContent(postId, "index.md");
+  return { props: { content } };
+}
+
+export default function Page(props) {
+  const { content } = props;
   return (
-    <ContentScope postId="hello-blog">
-      <MarkdownHTML path="index.md" />
+    <ContentScope postId={postId}>
+      <ContentHTML htmlAst={content.hast} />
     </ContentScope>
   );
 }
--- publishable/blog-engine/components/post.tsx
+++ publishable/blog-engine/components/post.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 export function ContentScope(props) {
-  return props.children;
+  const childProps = { postId: props.postId };
+  return React.Children.map(props.children, (c) =>
+    React.cloneElement(c, childProps)
+  );
 }
 
-export function MarkdownHTML(props) {
+export function ContentHTML(props) {
   return (
     <div>
       <h1>MarkdownHTML</h1>
