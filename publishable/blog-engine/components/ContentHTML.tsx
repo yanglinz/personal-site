20bc0446e1fc8c0466625d8037999efd12885798
Yanglin Zhao <yz1876@gmail.com>
2020-12-04 23:16:08 -0500

    Progress rendering content

--- publishable/blog-engine/components/ContentHTML.tsx
+++ publishable/blog-engine/components/ContentHTML.tsx
@@ -1,13 +1,21 @@
-// Render html
+import { h } from "preact";
+
+function ContentTree(node) {
+  let element = node.tagName || "div";
+  let attrs = {};
+
+  if (node.children) {
+    return h(element, attrs, node.children.map(ContentTree));
+  }
+
+  if (node.type === "text") {
+    return node.value;
+  }
+
+  return null;
+}
 
 export default function ContentHTML(props) {
   const { htmlAst } = props;
-
-  console.log(htmlAst);
-
-  return (
-    <div>
-      <h1>MarkdownHTML</h1>
-    </div>
-  );
+  return <ContentTree {...htmlAst} />;
 }
