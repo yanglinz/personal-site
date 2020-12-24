49856fa3eb5be5e28b9157369912fdecc946fdf7
Yanglin Zhao <yz1876@gmail.com>
2020-12-23 23:16:21 -0500

    Mark future type improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 
+type TODO = any;
+
 export async function getStaticProps() {
   const manifest = await import("../publishable/blog-engine/manifest");
   const posts = await manifest.getPostList();
   return { props: { posts } };
 }
 
-function Index(props) {
-  const { posts } = props;
+function Index(props: TODO) {
+  const posts: TODO = props.posts;
   return (
     <div>
       <h1>Yanglin Zhao</h1>
       <ul>
-        {posts.map((p) => (
+        {posts.map((p: TODO) => (
           <li>
             <a href={"/posts/" + p.id}>{p.title}</a>
           </li>
