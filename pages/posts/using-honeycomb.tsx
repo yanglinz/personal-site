c50a947a8e0b1327292cbe35578010e7b993ffd0
Yanglin Zhao <yanglinz@users.noreply.github.com>
2020-12-23 22:38:17 -0500

    Migrate more posts (#115)
    * Add component local reducer
    
    * Add the rest of the posts
    
    * Also format tsx

--- pages/posts/using-honeycomb.tsx
+++ pages/posts/using-honeycomb.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Image from "next/image";
 
 import ContentHTML from "../../publishable/blog-engine/components/ContentHTML";
 
-const postId = "hello-blog";
+const postId = "using-honeycomb";
 
 export async function getStaticProps() {
   const manifest = await import("../../publishable/blog-engine/manifest");
@@ -13,16 +12,5 @@ export async function getStaticProps() {
 
 export default function Page(props) {
   const { content } = props;
-  return (
-    <>
-      <Image
-        src="/content/hello-blog/banner.jpeg"
-        alt="Open road"
-        layout="responsive"
-        width={500}
-        height={300}
-      />
-      <ContentHTML htmlAst={content.ast} />
-    </>
-  );
+  return <ContentHTML htmlAst={content.ast} />;
 }
