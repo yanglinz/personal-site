0d114569cc02994164ce9518c7b85f2d93f3adb9
Yanglin Zhao <yz1876@gmail.com>
2020-12-25 16:40:34 -0500

    Include global css

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../src/styles/normalize.scss";
+import "../src/styles/highlight.scss";
+import "../src/styles/global.scss";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
