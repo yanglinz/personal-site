33690bba0af664b74899cc9e5c3ef92aca9c89db
Yanglin Zhao <yz1876@gmail.com>
2020-12-26 12:49:35 -0500

    Add title tag to pages

--- src/screens/Home/Layout.tsx
+++ src/screens/Home/Layout.tsx
@@ -1,11 +1,19 @@
+import Head from "next/head";
+
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 type TODO = any;
 
 export default function Layout(props: TODO) {
+  const description =
+    "I'm a software engineer and I make things that run in browsers. Come check out my thoughts on programming and technology.";
   return (
     <div className="Layout">
+      <Head>
+        <title>Yanglin Zhao</title>
+        <meta name="description" content={description} />
+      </Head>
       <Header />
       <div className="Layout-content">{props.children}</div>
       <Footer />
