1ce7c9d7b2bd71c92cfe70d90ed4d14d8eeafde1
Yanglin Zhao <yz1876@gmail.com>
2020-12-27 17:19:55 -0500

    Disable fathom for now

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,16 @@ import * as env from "../src/environment";
 import "../src/styles/index.scss";
 
 Router.events.on("routeChangeComplete", () => {
-  Fathom.trackPageview();
+  // Fathom.trackPageview();
 });
 
 export default function MyApp({ Component, pageProps }) {
   // Initialize Fathom when the app loads
   useEffect(() => {
-    Fathom.load(env.FATHOM_SITE_ID, {
-      url: "//beacon.yanglinzhao.com/tracker.js",
-      includedDomains: ["yanglinzhao.com"],
-    });
+    // Fathom.load(env.FATHOM_SITE_ID, {
+    //   url: "//beacon.yanglinzhao.com/tracker.js",
+    //   includedDomains: ["yanglinzhao.com"],
+    // });
   }, []);
 
   return <Component {...pageProps} />;
