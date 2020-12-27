eaa82ef3c3250509e21a4258e1c1115f16d3ff09
Yanglin Zhao <yz1876@gmail.com>
2020-12-26 21:58:18 -0500

    Add analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import React, { useEffect } from "react";
+import Router from "next/router";
+import * as Fathom from "fathom-client";
+import * as env from "../src/environment";
+
 import "../src/styles/index.scss";
 
+Router.events.on("routeChangeComplete", () => {
+  Fathom.trackPageview();
+});
+
 export default function MyApp({ Component, pageProps }) {
+  // Initialize Fathom when the app loads
+  useEffect(() => {
+    Fathom.load(env.FATHOM_SITE_ID, {
+      includedDomains: ["yanglinzhao.com"],
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
