36f21f3760aa60cebf153407e9eb7f00a14b520d
Yanglin Zhao <yz1876@gmail.com>
2021-03-27 11:27:17 -0400

    Consolidate intro and about

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { PostMetadata } from "@blog-engine/manifest";
 
 import Layout from "../src/screens/Home/Layout";
-import About from "../src/screens/Home/About";
 import Intro from "../src/screens/Home/Intro";
 import PostList from "../src/screens/Home/PostList";
 import Section from "../src/screens/Home/Section";
@@ -21,10 +20,6 @@ function Index(props: ComponentProps) {
   return (
     <Layout>
       <Intro />
-
-      <Section title="About">
-        <About />
-      </Section>
 
       <Section title="Blog Posts">
         <PostList posts={props.posts} />
--- src/screens/Home/Intro.tsx
+++ src/screens/Home/Intro.tsx
@@ -7,6 +7,16 @@ export default function HomeIntro() {
         <div className="Intro-border-top" />
         <div className="Intro-text-parent">
           <h1 className="Intro-text">Software Tinkerer</h1>
+          <p className="About-text">
+            Hello{" "}
+            <span role="img" aria-label="Waving hand">
+              &#x1F44B;.
+            </span>{" "}
+            I'm a software engineer based in Washington DC. I mainly make things
+            that run in browsers. I currently work at{" "}
+            <a href="https://www.pbs.org/">PBS</a> as a Principal Engineer to
+            help make high quality media accessible.
+          </p>
         </div>
       </div>
     </div>
