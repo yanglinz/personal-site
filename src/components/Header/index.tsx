40a5fbcf30d8d8b6433c925d882fedfe51b9d827
Yanglin Zhao <yz1876@gmail.com>
2021-03-26 23:15:03 -0400

    Migrate header styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,19 @@
 import Link from "next/link";
 
-import IconGithub from "../Icons/Github";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div className="Header">
+    <div className={styles.header}>
       <div className="l-wide">
-        <div className="Header-container">
-          <div className="Header-brand">
-            <h1 className="Header-title">
+        <div className="Headher-container">
+          <div>
+            <h1 className={styles.title}>
               <Link href="/">
                 <a className="Header-title-link">Yanglin Zhao</a>
               </Link>
             </h1>
-            <p className="Header-subtitle">hi (at) yanglinzhao.com</p>
-          </div>
-
-          <div className="Header-links">
-            <a href="https://github.com/yanglinz">
-              <IconGithub width={18} fill="#fff" />
-            </a>
+            <p className={styles.subtitle}>hi (at) yanglinzhao.com</p>
           </div>
         </div>
       </div>
