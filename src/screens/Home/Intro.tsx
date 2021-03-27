966f24fc100c04e613930003c4f5ed0b65c1ea69
Yanglin Zhao <yz1876@gmail.com>
2021-03-27 13:51:02 -0400

    Add basic styling to intro section

--- src/screens/Home/Intro.tsx
+++ src/screens/Home/Intro.tsx
@@ -1,23 +1,21 @@
 import styles from "./Intro.module.css";
 
-export default function HomeIntro() {
+export default function Intro() {
   return (
-    <div className="Intro">
+    <div className={styles.intro}>
       <div className="l-wide">
-        <div className="Intro-border-top" />
-        <div className="Intro-text-parent">
-          <h1 className="Intro-text">Software Tinkerer</h1>
-          <p className="About-text">
-            Hello{" "}
-            <span role="img" aria-label="Waving hand">
-              &#x1F44B;.
-            </span>{" "}
-            I'm a software engineer based in Washington DC. I mainly make things
-            that run in browsers. I currently work at{" "}
-            <a href="https://www.pbs.org/">PBS</a> as a Principal Engineer to
-            help make high quality media accessible.
-          </p>
-        </div>
+        <h1 className={styles.heading}>Software Tinkerer</h1>
+
+        <p className={styles.subheading}>
+          Hello{" "}
+          <span role="img" aria-label="Waving hand">
+            &#x1F44B;.
+          </span>{" "}
+          I'm a software engineer based in Washington DC. I mainly make things
+          that run in browsers. I currently work at{" "}
+          <a href="https://www.pbs.org/">PBS</a> as a Principal Engineer to help
+          make high quality media accessible.
+        </p>
       </div>
     </div>
   );
