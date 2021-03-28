22483efabf8adb2cbebd1ba917b07e52e11588e4
Yanglin Zhao <yz1876@gmail.com>
2021-03-28 11:47:27 -0400

    Style post list

--- src/screens/Home/PostList.tsx
+++ src/screens/Home/PostList.tsx
@@ -4,34 +4,50 @@ import { PostMetadata } from "@blog-engine/manifest";
 
 import styles from "./PostList.module.css";
 
-interface ComponentProps {
+interface PostListProps {
   posts: PostMetadata[];
 }
 
-export default function PostList(props: ComponentProps) {
+interface PostThumbnailProps {
+  post: PostMetadata;
+}
+
+function PostThumbnail(props: PostThumbnailProps) {
+  const { post } = props;
+  const width = 50;
+  const height = 50;
+  return (
+    <div
+      className={styles.postThumbnail}
+      style={{ width: width, height: height }}
+    >
+      {post.thumbnailImage ? (
+        <Image
+          src={post.thumbnailImage}
+          alt={`Thumbnail for ${post.title}`}
+          layout="intrinsic"
+          width={width}
+          height={height}
+        />
+      ) : null}
+    </div>
+  );
+}
+
+export default function PostList(props: PostListProps) {
   return (
     <>
       {props.posts.map((post) => (
         <div className={styles.post}>
-          <h3 className="Post-title">
+          <PostThumbnail post={post} />
+
+          <h3 className={styles.postTitle}>
             <Link href={post.urlPath}>
               <a>{post.title}</a>
             </Link>
           </h3>
 
-          {post.thumbnailImage ? (
-            <Image
-              src={post.thumbnailImage}
-              alt={`Thumbnail for ${post.title}`}
-              layout="intrinsic"
-              width={100}
-              height={100}
-            />
-          ) : (
-            <div style={{ width: 100, height: 100 }} />
-          )}
-
-          <p className="Post-date f-mono">
+          <p className={styles.postDate + " " + "f-mono"}>
             <Link href={post.urlPath}>
               <a>
                 <time>{post.date}</time>{" "}
