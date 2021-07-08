885b55c2e709e6048f1852de7f492ae91b80db6d
Yanglin Zhao <yz1876@gmail.com>
2021-07-07 23:13:21 -0400

    Tweak intro breakpoints

--- src/screens/Home/Intro.tsx
+++ src/screens/Home/Intro.tsx
@@ -1,12 +1,12 @@
 export default function Intro() {
   return (
-    <div className="Intro py-28">
+    <div className="Intro py-16 lg:py-28">
       <div className="l-wide">
-        <h1 className="m-0 text-gray-900 text-7xl font-bold leading-none">
+        <h1 className="m-0 text-gray-900 text-5xl font-bold leading-none lg:text-7xl">
           Software Tinkerer
         </h1>
 
-        <p className="m-0 mt-5 w-4/5 text-gray-900 text-5xl leading-snug">
+        <p className="m-0 mt-5 w-4/5 text-gray-900 text-3xl leading-snug lg:mt-7 lg:text-5xl lg:leading-snug">
           Hello{" "}
           <span role="img" aria-label="Waving hand">
             &#x1F44B;.
