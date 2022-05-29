d083bfaacb7f60158f786d23289b81d39290a69c
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-28 22:12:51 -0400

    Remove default tests

---
+++ src/components/Example.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Example() {
+  return <h1>Hello world!</h1>;
+}
+
+export default {
+  title: 'Example',
+  component: Example,
+};
+
+export const AnExample = () => <Example />;
