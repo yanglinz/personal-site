58977fd6000823823d87272d26de7a979a99a24c
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-30 15:46:45 -0400

    Add header and body

--- src/pages/LayoutAbout.mjs
+++ src/pages/LayoutAbout.mjs
@@ -2,8 +2,28 @@ import * as React from "react";
 
 const h = React.createElement;
 
+function Head() {
+  return h("head", null, [
+    h("meta", { key: 1, charSet: "utf-8" }),
+    h("meta", {
+      key: 2,
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    }),
+    h("link", { key: 3, rel: "manifest", href: "/site.webmanifest" }),
+    h("meta", { key: 4, name: "theme-color", content: "#84cc16" }),
+  ]);
+}
+
+function Body() {
+  return h("body", {}, h("div", null, "Hello world!"));
+}
+
 function LayoutAbout(props) {
-  return h("html", { lang: "en" }, h("div", null, "Hello world!"));
+  return h("html", { lang: "en" }, [
+    h(Head, { key: 1 }, null),
+    h(Body, { key: 2 }, null),
+  ]);
 }
 
 export default LayoutAbout;
