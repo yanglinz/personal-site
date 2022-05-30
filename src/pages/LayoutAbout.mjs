1c0d047288e64a0425b4c0ede9fd1b9c30b58ff4
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-30 16:09:38 -0400

    Move to layout base

--- src/pages/LayoutAbout.mjs
+++ src/pages/LayoutAbout.mjs
@@ -1,59 +1,11 @@
 import * as React from "react";
 
+import LayoutBase from "./LayoutBase.mjs";
+
 const h = React.createElement;
 
-// TODO: Migrate to LayoutBase.mjs
-function Head() {
-  return h("head", null, [
-    h("meta", { key: 1, charSet: "utf-8" }),
-    h("meta", {
-      key: 2,
-      name: "viewport",
-      content: "width=device-width, initial-scale=1",
-    }),
-    h("link", { key: 3, rel: "manifest", href: "/site.webmanifest" }),
-    h("meta", { key: 4, name: "theme-color", content: "#84cc16" }),
-
-    // Google fonts
-    h("link", {
-      key: 5,
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    }),
-    h("link", {
-      key: 7,
-      rel: "preconnect",
-      href: "https://fonts.gstatic.com",
-      crossOrigin: true,
-    }),
-    h("link", {
-      key: 7,
-      rel: "stylesheet",
-      href:
-        "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
-    }),
-
-    h("link", { key: 8, rel: "stylesheet", href: "/main.css" }),
-
-    h("script", {
-      key: 9,
-      defer: true,
-      ["data-domain"]: "yanglinzhao.com",
-      src: "https://plausible.io/js/plausible.js",
-    }),
-  ]);
-}
-
-// TODO: Migrate to LayoutBase.mjs
-function Body() {
-  return h("body", {}, h("div", null, "Hello world!"));
-}
-
 function LayoutAbout(props) {
-  return h("html", { lang: "en" }, [
-    h(Head, { key: 1 }, null),
-    h(Body, { key: 2 }, null),
-  ]);
+  return h(LayoutBase, null, h("div", null, "Hello world!"));
 }
 
 export default LayoutAbout;
---
+++ src/pages/LayoutBase.mjs
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+const h = React.createElement;
+
+function Head() {
+  const content = [
+    h("meta", { charSet: "utf-8" }),
+    h("meta", {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    }),
+    h("link", { rel: "manifest", href: "/site.webmanifest" }),
+    h("meta", { name: "theme-color", content: "#84cc16" }),
+
+    // Google fonts
+    h("link", {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    }),
+    h("link", {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "true",
+    }),
+    h("link", {
+      rel: "stylesheet",
+      href:
+        "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
+    }),
+
+    // Main scripts
+    h("link", { rel: "stylesheet", href: "/main.css" }),
+    h("script", {
+      defer: true,
+      ["data-domain"]: "yanglinzhao.com",
+      src: "https://plausible.io/js/plausible.js",
+    }),
+  ];
+
+  return h(
+    "head",
+    null,
+    content.map((e, i) => {
+      if (e[1]) {
+        e[1].key = i;
+      }
+
+      return e;
+    })
+  );
+}
+
+function Body(props) {
+  return h("body", {}, props.children);
+}
+
+function LayoutBase(props) {
+  return h("html", { lang: "en" }, [
+    h(Head, { key: 1 }, null),
+    h(Body, { key: 2 }, props.children),
+  ]);
+}
+
+export default LayoutBase;
