8d194f7505f4c7ffd71a140ce6e0fdd37a444462
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-30 17:15:03 -0400

    Add links

--- src/pages/SharedHeader.mjs
+++ src/pages/SharedHeader.mjs
@@ -2,37 +2,6 @@ import * as React from "react";
 
 const h = React.createElement;
 
-/*
-<div class="">
-  <div class="l-base">
-    <div class="flex items-center justify-between">
-      <div>
-        <h1 class="m-0 text-3xl leading-snug">
-          <a href="/">
-            <img width="28px" src="/brand-logo.png" alt="Yanglin Zhao" />
-          </a>
-        </h1>
-      </div>
-
-      <nav>
-        <a class="inline-block mr-2 text-gray-700 text-base font-bold" href="/"
-          >Home</a
-        >
-        <a
-          class="hidden inline-block mr-2 text-gray-700 text-base font-bold"
-          href="/about"
-        >
-          About
-        </a>
-        <a class="inline-block text-gray-700 text-base font-bold" href="/posts"
-          >Posts</a
-        >
-      </nav>
-    </div>
-  </div>
-</div>
-*/
-
 function NavLink(props) {
   return h(
     "a",
@@ -59,15 +28,20 @@ function SharedHeader(props) {
     )
   );
   const nav = h("nav", { key: 2 }, [
-    h(NavLink, { key: 1, href: "/" }, "Home"),
-    h(NavLink, { key: 2, href: "/about" }, "Home"),
-    h(NavLink, { key: 3, href: "/posts" }, "Home"),
+    h("div", {key: 1, className: "inline-block mr-2"}, h(NavLink, { href: "/" },"Home")),
+    h("div", {key: 2, className: "inline-block mr-2"}, h(NavLink, { href: "/about" },"About")),
+    h("div", {key: 3, className: "inline-block mr-0"}, h(NavLink, { href: "/posts" },"Posts")),
   ]);
 
   return h(
     "div",
     { className: "SharedHeader py-4 bg-green-50 lg:py-8" },
-    h("div", { className: "flex items-center justify-between" }, [brand, nav])
+
+    h(
+      "div",
+      { className: "l-base" },
+      h("div", { className: "flex items-center justify-between" }, [brand, nav])
+    )
   );
 }
 
