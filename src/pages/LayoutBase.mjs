fd227996070def74e76161a36c08eddb52e0109e
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-30 22:20:02 -0400

    Use jsx in header

--- src/pages/LayoutBase.mjs
+++ src/pages/LayoutBase.mjs
@@ -3,70 +3,52 @@ import * as React from "react";
 import SharedHeader from "./SharedHeader.mjs";
 import SharedFooter from "./SharedFooter.mjs";
 
-const h = React.createElement;
-
 function Head() {
-  const content = [
-    h("meta", { charSet: "utf-8" }),
-    h("meta", {
-      name: "viewport",
-      content: "width=device-width, initial-scale=1",
-    }),
-    h("link", { rel: "manifest", href: "/site.webmanifest" }),
-    h("meta", { name: "theme-color", content: "#84cc16" }),
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="/site.webmanifest" rel="manifest" />
+      <meta name="theme-color" content="#84cc16" />
 
-    // Google fonts
-    h("link", {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    }),
-    h("link", {
-      rel: "preconnect",
-      href: "https://fonts.gstatic.com",
-      crossOrigin: "true",
-    }),
-    h("link", {
-      rel: "stylesheet",
-      href:
-        "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
-    }),
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
 
-    // Main scripts
-    h("link", { rel: "stylesheet", href: "/main.css" }),
-    h("script", {
-      defer: true,
-      ["data-domain"]: "yanglinzhao.com",
-      src: "https://plausible.io/js/plausible.js",
-    }),
-  ];
-
-  return h(
-    "head",
-    null,
-    content.map((e, i) => {
-      return React.cloneElement(e, { ...e.props, key: i });
-    })
+      <link href="/main.css" rel="stylesheet" />
+      <script
+        defer
+        data-domain="yanglinzhao.com"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
+    </head>
   );
 }
 
 function Body(props) {
-  console.log("body");
-  return h(
-    "body",
-    {},
-    h("div", null, [
-      h(SharedHeader, { key: 1 }, null),
-      h("div", { key: 2 }, props.children),
-      h(SharedFooter, { key: 3 }, null),
-    ])
+  return (
+    <body>
+      <SharedHeader />
+      {props.children}
+      <SharedFooter />
+    </body>
   );
 }
 
 function LayoutBase(props) {
-  return h("html", { lang: "en" }, [
-    h(Head, { key: 1 }, null),
-    h(Body, { key: 2 }, props.children),
-  ]);
+  return (
+    <html lang="en">
+      <Head />
+      <Body>{props.children}</Body>
+    </html>
+  );
 }
 
 export default LayoutBase;
