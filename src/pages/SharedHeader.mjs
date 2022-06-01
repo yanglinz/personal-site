bb05e49aad46ba9226d7ff23c306aa000994bee4
Yanglin Zhao <hi@yanglinzhao.com>
2022-05-31 22:24:52 -0400

    Try new header style

--- src/pages/SharedHeader.mjs
+++ src/pages/SharedHeader.mjs
@@ -2,37 +2,19 @@ import * as React from "react";
 
 function SharedHeader(props) {
   return (
-    <div className="py-4 bg-green-50 lg:py-8">
+    <div className="lg:py-16">
       <div className="l-base">
         <div className="flex items-center justify-between">
           <div>
-            <h1 className="m-0 text-3xl leading-snug">
-              <a href="/">
-                <img width="28px" src="/brand-logo.png" alt="Yanglin Zhao" />
+            <h1 className="m-0 text-base leading-snug">
+              <a className="text-gray-800 text-base" href="/">
+                Yanglin Zhao
               </a>
             </h1>
-          </div>
 
-          <nav>
-            <a
-              className="inline-block mr-2 text-gray-700 text-base font-bold"
-              href="/"
-            >
-              Home
-            </a>
-            <a
-              className="hidden inline-block mr-2 text-gray-700 text-base font-bold"
-              href="/about"
-            >
-              About
-            </a>
-            <a
-              className="inline-block text-gray-700 text-base font-bold"
-              href="/posts"
-            >
-              Posts
-            </a>
-          </nav>
+            <p className="text-gray-400 text-base">Software Engineer</p>
+            <p className="text-gray-400 text-base">hi [at] yanglinzhao.com</p>
+          </div>
         </div>
       </div>
     </div>
