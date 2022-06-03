3f20d329d0351ea38979e7f597f75f76cc1dc9c4
Yanglin Zhao <hi@yanglinzhao.com>
2022-06-02 22:28:18 -0400

    Fix post list

--- src/pages/FragmentPostList.mjs
+++ src/pages/FragmentPostList.mjs
@@ -4,17 +4,17 @@ function Post(props) {
   const { title, url, formattedDate } = props;
 
   return (
-    <li class="border-gray-150 py-3 border-t md:flex md:items-center md:justify-between md:py-4 lg:py-6">
-      <h3 class="flex-1 pr-5">
-        <a href={url} class="text-lg font-bold lg:text-3xl">
+    <li className="border-gray-150 py-3 border-t md:flex md:items-center md:justify-between md:py-4 lg:py-6">
+      <h3 className="flex-1 pr-5">
+        <a href={url} className="text-lg font-bold lg:text-3xl">
           {title}
         </a>
       </h3>
 
-      <p class="flex-initial">
+      <p className="flex-initial">
         <a
           href="{{ post.url }}"
-          class="text-gray-400 font-mono text-xs md:text-sm"
+          className="text-gray-400 font-mono text-xs md:text-sm"
         >
           <time>{formattedDate}</time>
         </a>
@@ -27,12 +27,12 @@ function FragmentPostList(props) {
   const { posts } = props;
   return (
     <>
-      <div class="lg:py-18 py-14 md:py-16">
-        <div class="l-base">
-          <div class="gap-4 grid-cols-6 md:grid">
-            <div class="md:col-span-4 md:col-start-2">
-              <div class="border-gray-150 py-6 border-t-8">
-                <h2 class="pb-5 text-gray-500 text-xl font-bold lg:pb-7 lg:text-3xl">
+      <div className="lg:py-18 py-14 md:py-16">
+        <div className="l-base">
+          <div className="gap-4 grid-cols-6 md:grid">
+            <div className="md:col-span-4 md:col-start-2">
+              <div className="border-gray-150 py-6 border-t-8">
+                <h2 className="pb-5 text-gray-500 text-xl font-bold lg:pb-7 lg:text-3xl">
                   Blog Posts
                 </h2>
 
