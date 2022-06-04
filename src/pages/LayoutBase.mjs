1490a95089ab7e6d3688589130eb352a840c4543
Yanglin Zhao <hi@yanglinzhao.com>
2022-06-04 15:06:20 -0400

    We need extra head tags

--- src/pages/LayoutBase.mjs
+++ src/pages/LayoutBase.mjs
@@ -4,13 +4,16 @@ import SharedHeader from "./SharedHeader.mjs";
 import SharedHeaderOld from "./SharedHeaderOld.mjs";
 import SharedFooter from "./SharedFooter.mjs";
 
-function Head() {
+function Head(props) {
+  const { extraHead } = props;
   return (
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="/site.webmanifest" rel="manifest" />
       <meta name="theme-color" content="#84cc16" />
+
+      {extraHead}
 
       <link href="/main.css" rel="stylesheet" />
       <script
@@ -33,9 +36,10 @@ function Body(props) {
 }
 
 function LayoutBase(props) {
+  const { extraHead } = props;
   return (
     <html lang="en">
-      <Head />
+      <Head extraHead={extraHead} />
       <Body>{props.children}</Body>
     </html>
   );
