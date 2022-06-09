ecc5766f394163bb31c021fac6cf135b2a291c55
Yanglin Zhao <hi@yanglinzhao.com>
2022-06-08 21:49:15 -0400

    Add new header styles

--- src/templates/FragmentSharedHeader.mjs
+++ src/templates/FragmentSharedHeader.mjs
@@ -2,13 +2,21 @@ import * as React from "react";
 
 function FragmentSharedHeader() {
   return (
-    <div className="py-4 bg-green-50 lg:py-8">
+    <div className="py-12 bg-stone-900 lg:py-20">
       <div className="l-base">
         <div className="flex items-center justify-between">
           <div>
-            <h1 className="m-0 text-3xl leading-snug">Yanglin Zhao</h1>
-            <p>Software Engineer</p>
-            <p>hi [at] yanglinzhao.com</p>
+            <a href="/">
+              <h1 className="m-0 text-base text-white leading-snug">
+                Yanglin Zhao
+              </h1>
+              <p className="text-base text-stone-500 leading-snug">
+                Software Engineer
+              </p>
+              <p className="text-base text-stone-500 leading-snug">
+                hi [at] yanglinzhao.com
+              </p>
+            </a>
           </div>
         </div>
       </div>
