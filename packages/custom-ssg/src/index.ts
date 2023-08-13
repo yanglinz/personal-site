c2a01b801771794fa95287b3da3d9762c5686a9e
Yanglin Zhao <hi@yanglinzhao.com>
2023-08-13 05:57:42 -0400

    Rename function

--- packages/custom-ssg/src/index.ts
+++ packages/custom-ssg/src/index.ts
@@ -1,14 +1,14 @@
 import { Path, ContentManifest } from './types';
 
 /**
- * Generate a list of content manifests.
+ * Returns the list of content files (html,css,js,images) to be generated.
  */
-export function getContentTree(inputDirectory: Path): ContentManifest[] {
+export function getContentManifests(inputDirectory: Path): ContentManifest[] {
   return [];
 }
 
 /**
- * Generate files (mainly html and images) from a content manifest.
+ * Generate file(s) from a content manifest.
  */
 export function renderContent(content: ContentManifest) {
   // generate file(s) given 
