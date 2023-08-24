be51a53f2091a77196775d78d32edb53b8423192
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-08-24 16:24:40 -0400

    Add TODO comment

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 import markdoc from '@astrojs/markdoc';
+// TODO: Replace the official markdoc integration with our custom
+// integration if we can't customize the component outputs.
 // import markdoc from './astro/custom-markdoc-integration/src/index';
 
 // https://astro.build/config
