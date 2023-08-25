334311001a2ebf77def14874626ae2321977f11e
Yanglin Zhao <hi@yanglinzhao.com>
2023-08-24 21:07:29 -0400

    Try out astro as a markdoc based generator (#221)
    * Generate new astro site again
    
    * Formatting
    
    * Add astro
    
    * Add placeholder posts
    
    * Copy over existing markdoc integration
    
    * Remove problematic typescript config
    
    * Nest markdown files
    
    * Render collection content with astro
    
    * Move to deps
    
    * Minimal README
    
    * Add TODO comment
    
    * Add existing posts
    
    * Use tailwind
    
    * Add tailwind
    
    * Try adding global styles
    
    * Remove fluff from index.html
    
    * Use layout in post too
    
    * Ignore vendered files
    
    * Use index page
    
    * Remove global style and copy over meta tags
    
    * Copy over public
    
    * Add TODOs
    
    * Import tailwind
    
    * Add global components
    
    * Remove custom integration
    
    * Add astro plugin
    
    * Format astro files

---
+++ packages/www/README.md
@@ -0,0 +1,5 @@
+# Personal Site
+
+Source code for my personal site.
+
+[https://yanglinzhao.com](https://yanglinzhao.com).
---
+++ packages/www/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import markdoc from "@astrojs/markdoc";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), markdoc()],
+});
