ee5aae836001e4da53175704a2fd740935fe860e
Yanglin Zhao <hi@yanglinzhao.com>
2023-08-27 14:13:28 -0400

    Use silly hack

---
+++ packages/www/bin/build.sh
@@ -0,0 +1,11 @@
+
+yarn astro build
+
+# This is a silly hack specifically for vercel builds, where
+# dynamically generated image assets in `public` dir is not
+# taken into account during the final output generation.
+# By adding a second build, we make sure that `public` is properly
+# populated with images at the beginning of the build.
+if [[ -z "${CI}" ]]; then
+  yarn astro build
+fi
