0bf628265599ccb799791ec257a8bd3358ad2ca1
Yanglin Zhao <hi@yanglinzhao.com>
2023-08-27 19:56:40 -0400

    Add sitemap

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), markdoc()],
+  site: 'https://yanglinzhao.com',
+  integrations: [markdoc(), tailwind(), sitemap()],
 });
