7acdb218cc57d12fc90b7138e8b18ec929a32b74
Yanglin Zhao <hi@yanglinzhao.com>
2023-08-27 20:05:53 -0400

    Generate sitemap (#226)
    * Add sitemap
    
    * Add robot.txt

--- packages/www/bin/build.sh
+++ packages/www/bin/build.sh
@@ -9,6 +9,6 @@ yarn astro build
 # taken into account during the final output generation.
 # By adding a second build, we make sure that `public` is properly
 # populated with images at the beginning of the build.
-if [[ "$CI" == "1" ]]; then
+if [[ "${CI:-0}" == "1" ]]; then
   yarn astro build
 fi
