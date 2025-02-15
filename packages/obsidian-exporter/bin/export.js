a350ee17a5ad35e46766ae737b298ab222d421a0
Yanglin Zhao <hi@yanglinzhao.com>
2025-02-14 23:03:34 -0500

    Add script to export to markdown

--- packages/obsidian-exporter/bin/export.js
+++ packages/obsidian-exporter/bin/export.js
@@ -1,18 +1,42 @@
 const path = require('node:path');
 const fsp = require('node:fs/promises');
 
-async function getManifest() {
-    const postsPath = path.resolve(process.cwd(), '../../..');
-    
+async function getAllPosts() {
+    const postsPath = path.resolve(process.cwd(), '../www/src/content/posts');
+    let postDirs = await fsp.readdir(postsPath, { withFileTypes: true });
+    postDirs = postDirs.filter(d => d.isDirectory());
+    return postDirs.map(d => ({
+        slug: d.name,
+        path: path.join(d.path, d.name)
+    }))
 }
 
-async function exportPost() {
-  
+async function exportPost(post) {
+    // TODO: Convert to markdown
+    let content = await fsp.readFile(path.resolve(post.path, 'index.mdoc'))
+    let outDir = path.join(process.cwd(), 'dist', post.slug);
+    try {
+        await fsp.mkdir(outDir, { recursive: true })
+    } catch(e) {
+        // do nothing
+    }
+
+    await fsp.writeFile(path.join(outDir, `${post.slug}.md`), content);
 }
 
-function exportAll() {
-    // get a list of posts
-    // export each post
+async function exportAll() {
+    const posts = await getAllPosts();
+    try {
+      await fsp.mkdir('./dist');
+    }
+    catch(e) {
+        // do nothing
+    }
+    for (let p of posts) {
+        await exportPost(p);
+    }
+
+    // TODO: Run prettier
 }
 
 exportAll();
