ae6585e8218f598a68515dcdb73dcf4fffd03404
Yanglin Zhao <hi@yanglinzhao.com>
2025-02-14 22:42:04 -0500

    Add note

---
+++ packages/obsidian-exporter/bin/export.js
@@ -0,0 +1,18 @@
+const path = require('node:path');
+const fsp = require('node:fs/promises');
+
+async function getManifest() {
+    const postsPath = path.resolve(process.cwd(), '../../..');
+    
+}
+
+async function exportPost() {
+  
+}
+
+function exportAll() {
+    // get a list of posts
+    // export each post
+}
+
+exportAll();
