046b9ed992a821abe5186ebe59ff9211e158864d
Yanglin Zhao <hi@yanglinzhao.com>
2025-02-18 21:24:28 -0500

    Use pnpm instead of yarn (#242)
    * Use pnpm
    
    * Replace all references to yarn

--- packages/www/bin/build.sh
+++ packages/www/bin/build.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-yarn astro build
+pnpm exec astro build
 
-# This is a silly hack specifically for vercel builds, where
+# This is a silly hack specifically for Vercel builds, where
 # dynamically generated image assets in `public` dir is not
 # taken into account during the final output generation.
 # By adding a second build, we make sure that `public` is properly
 # populated with images at the beginning of the build.
 if [[ "${CI:-0}" == "1" ]]; then
-  yarn astro build
+  pnpm exec astro build
 fi
